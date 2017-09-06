99c915620156dddd6a1dc5bd1f77cc55cbb4792a
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-06 12:17:39 +0700

    :tada: Initial Commit

---
+++ ADT/midterm_1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct info {
+    char name[20];
+    int age;
+} info_type; // 24 bytes
+
+typedef union data {
+    int myInt;
+    char myString[100];
+} data_type; // 100 bytes
+
+typedef struct myStruct {
+    info_type myInfo[15];
+    data_type myData[20];
+} type_abc; // 24*15 + 100*20 = 2,360 bytes
+
+int main(){
+	type_abc *p = (type_abc*)calloc(sizeof(type_abc), 50);
+	int *init_p_position = p;
+	printf("sizeof(p) = %d\n", sizeof(p));
+	printf("&p[0] = %p (%d)\n", &p[0], &p[0]);
+	printf("&p[1] = %p (%d)\n", &p[1], &p[1]);
+	printf("&p[1]-&p[0] = %p (%d)\n", (int)&p[1]-(int)&p[0], (int)&p[1]-(int)&p[0]);
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 2017-sophomore-semester-1
-Resources for CPE Sophomore Semester 1
+# Resources for CPE Sophomore Semester 1
